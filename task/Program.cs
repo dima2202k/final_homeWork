2231da367ac9e75ad3fb9dfd415e127727ebc196
dmitriy komov <dima22.02.kom@mail.ru>
2024-05-07 21:06:44 +0300

    add programm

---
+++ task/Program.cs
@@ -0,0 +1,20 @@
+﻿
+void main () {
+
+    string [] stringArray = new string [] {"Hello", "2", "end", "world", "asd"};
+    string [] newStringArray = [];
+
+    foreach (string item in stringArray)
+    {
+        if (item.Length <= 3)
+        {
+            string [] tempArray = new string [newStringArray.Length];
+            Array.Copy(newStringArray, tempArray, newStringArray.Length);
+            newStringArray = new string [newStringArray.Length + 1]; 
+            Array.Copy(tempArray, newStringArray, tempArray.Length);
+            newStringArray[newStringArray.Length - 1] = item;
+        }      
+    }
+}
+
+main();
